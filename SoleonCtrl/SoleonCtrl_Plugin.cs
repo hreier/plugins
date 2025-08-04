cd7b47e333d28db1bee65768c288d102ec74b15c
hreier <hreier@gmail.com>
2025-08-04 20:46:53 +0200

    Added SoleonCtrl (dll); first Plugin Skeleton

---
+++ SoleonCtrl/SoleonCtrl_Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Text;
+using MissionPlanner.Utilities;
+using MissionPlanner.Controls;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using MissionPlanner;
+using System.Drawing;
+using GMap.NET.WindowsForms;
+using MissionPlanner.GCSViews;
+using MissionPlanner.Maps;
+using System.Timers;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using System.Drawing.Drawing2D;
+
+
+namespace SoleonCtrl_Plugin
+{
+    internal class SoleonCtrl_Plugin : MissionPlanner.Plugin.Plugin
+    {
+        public override string Name { get; } = "SoleonCtrl Plugin";
+        public override string Version { get; } = "0.0.0";
+        public override string Author { get; } = "soleon.it";
+        public override bool Init() { return true; }
+        public override bool Loaded()
+        {
+            return true;
+        }
+        public override bool Exit() { return true; }
+
+
+    }
+}
--- readme.md
+++ readme.md
@@ -1,22 +1,33 @@
 # Plugins for Missionplanner 
 This repo collects the Missionplanner plugins for Soleon
 
-## soleon_sprayer_ctrl
+## ./so_data
+Data used to generate the Plugins (e.g. pictures)  
+
+
+## soleon_sprayer_ctrl.cs
 Plugin to show the sprayer status and for basic control of it  
 >  [!NOTE]  
 >  This is source code plugin.
   
 
-## soleon_diagnosis
+## soleon_diagnosis.cs
 Plugin for diagnosis, service, maintenance and debug  
 >  [!NOTE]  
 >  This is source code plugin.
+  
+## ./SoleonCtrl
+Plugin to show the sprayer status and for basic control of it  
+>  [!NOTE]  
+>  This is compiled plugin (dll).
   
 
 ## Links  
 ### Documentation / Video  
 https://discuss.ardupilot.org/t/writing-a-mission-planner-plugin/136137  
 https://www.youtube.com/watch?v=UEVgGTPAhw   
+
+https://github.com/EosBandi/PluginSkeleton  (compiled plugin; dll)  
   
 ### Airpixel plugins  
 https://airpixel.cz/docs/missionplanner-plugin-for-mavporter/  
